0604135cda9bd4a0a65a832c7195637b78c5b33f
miguelromeroglb <miguel.romero@globant.com>
2022-05-15 11:26:37 -0500

    add button to storybook

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import { Button } from './Button';
+import Button from '../components/Button/Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -18,23 +14,26 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  variant: 'primary',
+  children: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  variant: 'secondary',
+  children: 'Button'
 };
 
 export const Large = Template.bind({});
 Large.args = {
+  variant: 'primary',
   size: 'large',
-  label: 'Button',
+  children: 'Button',
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Normal = Template.bind({});
+Normal.args = {
+  variant: 'primary',
+  size: 'normal',
+  children: 'Button',
 };
