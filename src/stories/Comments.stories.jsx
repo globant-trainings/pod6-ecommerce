ae27e319f98ac58ecda32b7a5911d4d21bb95b56
Carlos Ismael Ponce Trinidad <iponce@sofi.org>
2022-05-19 15:22:22 -0500

    added the comments component to storybook

---
+++ src/stories/Comments.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Comments from '../components/Comments/Comments';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'CommentsComponent',
+  component: Comments,
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Comments {...args} />;
+
+export const CommentsComponent = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+CommentsComponent.args = {
+  numberOfComments: 2,
+};
+
