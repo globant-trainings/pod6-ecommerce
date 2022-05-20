72d0c765b18980ecae1479b0e0af2e9210c43ec7
LuisDiaz95 <47155488+LuisDiaz95@users.noreply.github.com>
2022-05-20 01:42:47 -0600

    Add tests to rating component

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './rating.css';
 
+function getClassName(index, rating) {
+  if(index <= rating) return "star on"
+  else if(index - 0.5 <= rating) return "star half"
+  return "star"
+}
+
 const Rating = ({
   rating
 }) => {
   return (
-    <div className="star-rating">
+    <div data-testid="star-rating">
       {[...Array(5)].map((star, index) => {
         index += 1;
         return (
           <span
             key={index}
-            className={index <= rating ? "star on" : (index-0.5) <= rating ? "star half" : "star"}
+            className={getClassName(index, rating)}
           >
           </span>
         );
