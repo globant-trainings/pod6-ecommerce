c0f7170cc808f1cbe02be9280bd4783160d2766f
So Lym <solym@MacBook-Air-de-Sofia.local>
2022-06-05 15:35:07 -0500

    checkoutReady

--- src/components/checkoutList/CheckoutList.jsx
+++ src/components/checkoutList/CheckoutList.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./checkoutList.css";
 import trash from "../../assets/trash.jpg";
+//import CartContext from "../../store/cart/CartContext";
+//import { useContext } from "react";
 
 const CheckoutList = ({ products }) => {
+  //const { removeFromCart } = useContext(CartContext);
+  const [cartItems, setCartItems] = useState(products);
+  const remove = (product) => {
+    //removeFromCart(product);
+    setCartItems(cartItems.filter((item) => item.id !== product.id));
+  };
+
   return (
     <div>
-      {products.map((element) => {
+      {cartItems.map((product) => {
         return (
           <div className="row__checkout">
             <img alt="coffee" src={require("../../assets/coffee.png")} />
-            <p>{element.name}</p>
-            <p>${element.price}</p>
-            <img alt="trash" src={trash} />
+            <p>{product.name}</p>
+            <p>${product.price}</p>
+            <p>x{product.quantity}</p>
+            <img onClick={() => remove(product)} alt="trash" src={trash} />
           </div>
         );
       })}
