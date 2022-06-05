868678e732b22b1575edfe36969fa892b1eaa3f9
So Lym <solym@MacBook-Air-de-Sofia.local>
2022-06-05 15:52:13 -0500

    finishedStatusComponent

--- src/components/CartStatus/CartStatus.jsx
+++ src/components/CartStatus/CartStatus.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import cart from "../../assets/cart.png";
+import circle from "../../assets/circle.png";
 import CartContext from "../../store/cart/CartContext";
 import { useContext } from "react";
+import "./cartStatus.css";
 
 const CartStatus = () => {
-  const { cartItems } = useContext(CartContext);
+  const { itemCount } = useContext(CartContext);
   return (
-    <div>
-      <p>{cartItems}</p>
+    <div className="container__cartStatus">
       <img src={cart} alt="cart" />
+      <div className="container__status">
+        <img src={circle} alt="circle" />
+        <p className="container__counter">{itemCount}</p>
+      </div>
     </div>
   );
 };
